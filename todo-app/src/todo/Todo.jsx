b41c21fd1bb31d3b2bd11f1198fa23930337ab00
Muhammed Fayiz <fayiz149165@gmail.com>
2025-10-24 14:05:44 +0530

    add a delete button

--- todo-app/src/todo/Todo.jsx
+++ todo-app/src/todo/Todo.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React ,{useState}from 'react'
+import './Todo.css';
 const Todo = () => {
+  const [state,setState]=useState([])
+  const [task,setTask]=useState()
+ 
+  const AddTask=()=>{
+    console.log(task);
+    
+    setState([...state,task])
+    setTask('')
+  }
+  
+  const AddTag=(e)=>{
+    setTask(e.target.value)
+
+  }
+
+  const edit=()=>{
+   
+  }
+ const deletetask = (index) => {
+  const newTask = state.filter((item, i) => i !== index)
+  setState(newTask)
+}
   return (
-    <div>
-      <h1>tdoo</h1>
+ <div className="todo-container">
+
+
+    {state.map((item,index)=>{
+      return <><h1 key={index}>{item}</h1>
+
+
+   
+        <button onClick={() => deletetask(index)}>del</button>
+      <button onClick={edit}>edit</button>
+      </>
+    })}
+ 
+     <input type="text" name="task" value={task} onChange={AddTag}/>
+      <button onClick={AddTask}>add</button>
     </div>
   )
 }
