fc207d6cc5ff0838ad93440853ca40a0510d61c7
Muhammed Fayiz <fayiz149165@gmail.com>
2025-10-26 22:25:02 +0530

    add styles

--- todo-app/src/todo/Todo.jsx
+++ todo-app/src/todo/Todo.jsx
@@ -1,46 +1,70 @@
-import React ,{useState}from 'react'
+import React, { useState } from 'react';
 import './Todo.css';
+
 const Todo = () => {
-  const [state,setState]=useState([])
-  const [task,setTask]=useState()
- 
-  const AddTask=()=>{
-    console.log(task);
-    
-    setState([...state,task])
-    setTask('')
-  }
-  
-  const AddTag=(e)=>{
-    setTask(e.target.value)
+  const [task, setTask] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
 
-  }
+  const addTask = () => {
+    if (inputValue.trim() === '') return;
+    setTask([...task, inputValue]);
+    setInputValue('');
+  };
 
-  const edit=()=>{
-   
-  }
- const deletetask = (index) => {
-  const newTask = state.filter((item, i) => i !== index)
-  setState(newTask)
-}
+  const deleteTask = (index) => {
+    const newTasks = task.filter((_, i) => i !== index);
+    setTask(newTasks);
+    setEditIndex(null);
+    setInputValue('');
+  };
+
+  const editTask = (index) => {
+    setInputValue(task[index]);
+    setEditIndex(index);
+  };
+
+  const changeTask = (index) => {
+    if (editIndex === null || inputValue.trim() === '') return;
+    const updatedTasks = [...task];
+    updatedTasks[index] = inputValue.trim();
+    setTask(updatedTasks);
+    setEditIndex(null);
+    setInputValue('');
+  };
+
   return (
- <div className="todo-container">
+    <div className='todo-container'>
+      <h1 className='todo-title'>My To-do List</h1>
 
+      <div className='todo-input-area'>
+        <input
+          type='text'
+          className='todo-input'
+          placeholder='Enter your task'
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        {editIndex !== null ? (
+          <button className='todo-button update' onClick={() => changeTask(editIndex)}>Update</button>
+        ) : (
+          <button className='todo-button add' onClick={addTask}>Add</button>
+        )}
+      </div>
 
-    {state.map((item,index)=>{
-      return <><h1 key={index}>{item}</h1>
-
-
-   
-        <button onClick={() => deletetask(index)}>del</button>
-      <button onClick={edit}>edit</button>
-      </>
-    })}
- 
-     <input type="text" name="task" value={task} onChange={AddTag}/>
-      <button onClick={AddTask}>add</button>
+      <div className='todo-list'>
+        {task.map((item, index) => (
+          <div className='todo-item' key={index}>
+            <span className='todo-text'>{item}</span>
+            <div className='todo-actions'>
+              <button className='edit-btn' onClick={() => editTask(index)}>Edit</button>
+              <button className='delete-btn' onClick={() => deleteTask(index)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
